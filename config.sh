0ac9e4039a2132e7a7f87d53d6806a7290f7549f
superlynx <supercatexpert@gmail.com>
2014-06-13 19:57:37 +0800

    Enable PCI/PCI-E support and added Intel(R) wireless card support.

---
+++ config.sh
@@ -0,0 +1,6 @@
+export ARCH="arm"
+export CORES="24"
+export CROSS_COMPILE="arm-linux-gnueabihf-"
+export PATH="/opt/gcc-linaro-arm-linux-gnueabihf-4.8-2013.06_linux/bin:/opt/gcc-linaro-arm-linux-gnueabihf-4.8-2013.06_linux/bin:$PATH"
+
+make menuconfig
