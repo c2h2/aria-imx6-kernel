5b3fcd55b79513100ab692cf0507ec7f6f401b73
Yiling Cao <yiling.cao@gmail.com>
2015-04-03 18:23:53 +0800

    add install scripts.

---
+++ build.sh
@@ -0,0 +1,29 @@
+export ARCH="arm"
+export CORES="24"
+export CROSS_COMPILE="arm-linux-gnueabihf-"
+export PATH="/opt/gcc-linaro-arm-linux-gnueabihf-4.8-2013.06_linux/bin:/opt/gcc-linaro-arm-linux-gnueabihf-4.8-2013.06_linux/bin:$PATH"
+
+make -j16 uImage LOADADDR=0x10008000
+make -j16 modules
+
+make imx6dl-sabresd.dtb
+make imx6q-sabresd.dtb
+
+rm -rf deploy
+mkdir -p deploy/mod
+
+cp arch/arm/boot/uImage deploy/
+cp arch/arm/boot/dts/imx6q-sabresd.dtb deploy/
+cp arch/arm/boot/dts/imx6dl-sabresd.dtb deploy/
+
+make modules_install INSTALL_MOD_PATH=deploy/mod
+
+mkdir -p deploy/mod/usr/src/linux
+make headers_install INSTALL_HDR_PATH=deploy/mod/usr/src/linux
+
+make firmware
+make firmware_install INSTALL_MOD_PATH=deploy/mod
+
+cd deploy/mod
+tar -czf ../mods.tar.gz ./
+cd ../..
